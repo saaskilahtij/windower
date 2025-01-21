56bb44e0e2fc6b7b2ea13bb3662c420b6fb468d0
johan <saaskilahtijohan@gmail.com>
2025-01-21 21:49:48 +0200

    Init, argparsing & ASCII art

---
+++ windower.py
@@ -0,0 +1,42 @@
+"""
+File: windower.py
+Authors: Johan Sääskilahti, Atte Rajavaara, Minna Repo, Topias Hämäläinen
+Description: This tool was designed to create windows from preprocessed JSON data
+"""
+
+import argparse
+import sys
+
+
+DESC = r"""
+     _     _     _   O             ___        __     _     _     _   ____
+     \    | \    |  /|\  __    _  | ___\    /    \    \    | \    |  |   \
+      \   |  \   |   |   | \   |  | ^ ^ \  / 0  0 \    \   |  \   |  |____\
+       \  |   \  |   |   |  \  |  |   _ /  \   \  /     \  |   \  |  |   \
+        \ |    \ |   |   |   \ |  | ___/    \ __ /       \ |    \ |  |    \
+         \|     \|   |   |    \|  |___/      \__/         \|     \|  |     \
+                         windows made quick and easy
+"""
+
+def handle_args():
+    """
+    This function parses the arguments
+
+    Returns:
+        Parsed args
+    Note:
+        The return can seem a bit funny. However, it makes sure that
+        if the tool is ran without args, it will print the help message.
+        This is because argparse does not support this out of the box.
+    """
+    parser = argparse.ArgumentParser(description=DESC, prog='windower.py',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbosity',
+                        default=False)
+
+    return parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+if __name__ == '__main__':
+
+    handle_args()
+    print("Hello windower")
